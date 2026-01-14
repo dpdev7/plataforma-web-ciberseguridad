1ba705c1250700f4936caa36ff28a8d3a7ba0dc3
Julian Gutierrez <juligumer03@gmail.com>
2026-01-14 15:59:25 -0500

    Subi un archivo llamado instrucciones.ts que les dira los pasos para ejecutar el backend

---
+++ BACKEND/instructions.ts
@@ -0,0 +1,26 @@
+/*PARA INSTALAR EL ENTORNO VIRTUAL 
+
+desde la carpeta BACKEND, ejecunten el siguiente comando:
+
+- python -m venv env 
+
+para mac:
+
+- python3 -m venv env
+
+luego de que se les cree la carpeta env se dirigen a la ruta env\Scripts\ y ejecutrar activate y se les activara el entorno virtual, aparecera un (env)
+
+para mac en env/bin/ y ejectutar activate
+
+luego deben instalar django con el siguiente comando:
+
+- pip install django
+
+
+** PARA CORRER EL SERVIDOR **
+
+desde la carpeta backend, ejecutar el siguiente comando:
+
+python manage.py runserver
+
+el servidor les correra en el puerto 8000*/
